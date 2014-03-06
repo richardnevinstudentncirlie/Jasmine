44ae6b42c919a9248f96683e313a03bbd04036c4
Richard Nevin <richard.nevin@outlook.com>
2014-03-06 00:19:06 +0000

    Added Skel

---
+++ app/js/Converter.js
@@ -0,0 +1,25 @@
+var Converter = (function() 
+{  
+
+	var converterValue = 2.2;
+	return 
+	{ 
+  
+		convertFromPoundsToKIlo: function(weight) 
+		{ 
+			var num = parseInt(weight); 
+			if (isNaN(num) == true) 
+			{ 
+				throw new Error ("Not a number");  
+			}
+
+			num = num / converterValue; 
+			return Math.round(num); 
+		}, 
+	
+		convertFromMetricToImperial: function(weight)
+		{
+			return weight / converterValue; 
+		} 
+	} 
+}())
--- test/spec/ConverterSpec.js
+++ test/spec/ConverterSpec.js
@@ -1,10 +1,16 @@
 describe ("My Converter", function() {
+
     it("should convert imperial weight to metric", function() {
+	
         expect (Converter.convertFromImperialToMetric(2.2)).toBe(1);
 
-    })
-    it("should be able to deal with strings", function() {
-
+    })it("should be able to deal with strings", function() {
+	
         expect (function() {Converter.convertFromImperialToMetric("hello")}).toThrow(new Error("Not a number"));
+		
     })
 })
+
+
+
+
